d515d343ad4a5a20f309101b9228dc0174d76c59
itpcmc2024 <itpcmc2024@gmail.com>
2026-07-23 11:30:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   // วาง URL ที่ลงท้ายด้วย /exec จากการ Deploy Google Apps Script Web App
-  API_URL: "https://script.google.com/macros/s/AKfycbx1PfhSNxh1HJOU5CG6ZpOSqPa9u4Lbl_AuNQ715PKlKttV69Cq6Zv2Igee1ad9eVvTQQ/exec",
+  API_URL: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
 };
